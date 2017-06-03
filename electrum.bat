9dba41898f8657a40d4d0f85806e2f981cf753b7
sontol <fake01@gmail.com>
2017-06-04 05:23:30 +0700

    Add TCP/SSL Port

--- electrum.bat
+++ electrum.bat
@@ -1,6 +1,8 @@
 PATH=C:\Users\User\node_modules\level\node_modules\leveldown\deps\leveldb\Release;%PATH%
 SET DB_DIRECTORY=c:\users\user\downloads\electrum
 SET DAEMON_URL=http://user:password@127.0.0.1:18332/
+SET TCP_PORT=50001
+SET SSL_PORT=50002
 SET SSL_CERTFILE=C:\Users\User\Downloads\openssl-1.0.2k-x64_86-win64\server.crt
 SET SSL_KEYFILE=C:\Users\User\Downloads\openssl-1.0.2k-x64_86-win64\server.key
 SET COIN=Bitcoin
