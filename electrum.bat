44d17d862690c639204951866e69471225c9e369
sontol <fake01@gmail.com>
2017-06-04 05:24:19 +0700

    Change DAEMON_URL to match windows.md

--- electrum.bat
+++ electrum.bat
@@ -1,6 +1,6 @@
 PATH=C:\Users\User\node_modules\level\node_modules\leveldown\deps\leveldb\Release;%PATH%
 SET DB_DIRECTORY=c:\users\user\downloads\electrum
-SET DAEMON_URL=http://user:password@127.0.0.1:18332/
+SET DAEMON_URL=http://user:password@127.0.0.1:18333/
 SET TCP_PORT=50001
 SET SSL_PORT=50002
 SET SSL_CERTFILE=C:\Users\User\Downloads\openssl-1.0.2k-x64_86-win64\server.crt
